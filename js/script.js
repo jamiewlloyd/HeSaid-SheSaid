b6b9cc9334d0d4cba1e07c4cd70d6a091029dd8c
Jamie Lloyd <jamielloyd.work@gmail.com>
2020-05-12 13:28:02 +0100

    Update script.js
    Updating the javascript to ES6

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
 const NUMBER_OF_BUTTONS = 9;
 
-var heInput = document.getElementById("he");
-var sheInput = document.getElementById("she");
-var lnHeInput = document.getElementById("lnHe");
-var lnSheInput = document.getElementById("lnShe");
-var theyInput = document.getElementById("they");
-var heSaidInput = document.getElementById("heSaid");
-var sheSaidInput = document.getElementById("sheSaid");
-var onlookersInput = document.getElementById("onlookers");
-var tomorrowInput = document.getElementById("tomorrow");
+let heInput = document.getElementById("he");
+let sheInput = document.getElementById("she");
+let lnHeInput = document.getElementById("lnHe");
+let lnSheInput = document.getElementById("lnShe");
+let theyInput = document.getElementById("they");
+let heSaidInput = document.getElementById("heSaid");
+let sheSaidInput = document.getElementById("sheSaid");
+let onlookersInput = document.getElementById("onlookers");
+let tomorrowInput = document.getElementById("tomorrow");
 
-var heBtn = document.getElementById("heBtn");
-var sheBtn = document.getElementById("sheBtn");
-var lnHeBtn = document.getElementById("lnHeBtn");
-var lnSheBtn = document.getElementById("lnSheBtn");
-var theyBtn = document.getElementById("theyBtn");
-var heSaidBtn = document.getElementById("heSaidBtn");
-var sheSaidBtn = document.getElementById("sheSaidBtn");
-var onlookersBtn = document.getElementById("onlookersBtn");
-var tomorrowBtn = document.getElementById("tomorrowBtn");
-var generateBtn = document.getElementById("generator");
-var resetBtn = document.getElementById("reset");
+const heBtn = document.getElementById("heBtn");
+const sheBtn = document.getElementById("sheBtn");
+const lnHeBtn = document.getElementById("lnHeBtn");
+const lnSheBtn = document.getElementById("lnSheBtn");
+const theyBtn = document.getElementById("theyBtn");
+const heSaidBtn = document.getElementById("heSaidBtn");
+const sheSaidBtn = document.getElementById("sheSaidBtn");
+const onlookersBtn = document.getElementById("onlookersBtn");
+const tomorrowBtn = document.getElementById("tomorrowBtn");
+const generateBtn = document.getElementById("generator");
+const resetBtn = document.getElementById("reset");
 
-var count = 0;
+let count = 0;
 
-
-function processInput(event) {
-    var inputValue = event.path[1].childNodes[3].value;
+const processInput = (event) => {
+    let inputValue = event.path[1].childNodes[3].value;
     if (inputValue.length > 0) {
         event.path[1].classList.add("hide");
         count++;
@@ -34,27 +33,27 @@ function processInput(event) {
     generateButton();
 };
 
-function generateButton() {
+const generateButton = () => {
     if (count === NUMBER_OF_BUTTONS) {
         generateBtn.classList.toggle("hide");
     }
 };
 
-function generateStory() {
+const generateStory = () => {
     generateBtn.classList.toggle("hide");
-    document.getElementById("story").innerHTML = "He was " + heInput.value + ". She was " + sheInput.value +
-        ". Last night he " + lnHeInput.value + " while she " + lnSheInput.value + ". Today they were "
-        + theyInput.value + ". He said " + heSaidInput.value + " and she said " + sheSaidInput.value + ". Onlookers said "
-        + onlookersInput.value + ". Tomorrow they will " + tomorrowInput.value + ".";
+    document.getElementById("story").innerHTML = `He was ${heInput.value}. She was ${sheInput.value}. 
+    Last night he ${lnHeInput.value} while she ${lnSheInput.value}. Today they were ${theyInput.value}. 
+    He said ${heSaidInput.value} and she said ${sheSaidInput.value}. Onlookers said ${onlookersInput.value}. 
+    Tomorrow they will ${tomorrowInput.value}.`;
     document.getElementById("story").classList.toggle("hide");
     resetBtn.classList.toggle("hide");
 }
 
-function pageReset() {
-    var children = document.getElementById("inputs").children;
+const pageReset = () => {
+    let children = document.getElementById("inputs").children;
     document.getElementById("story").classList.toggle("hide");
     resetBtn.classList.toggle("hide");
-    for (var i = 0; i < children.length; i++) {
+    for (let i = 0; i < children.length; i++) {
         children[i].classList.toggle("hide");
         children[i].childNodes[3].value = "";
 
